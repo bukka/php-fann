fb2e1c12609774396bc9ec8598f1579dc6792af2
Jakub Zelenka <jakub.zelenka@gmail.com>
2013-02-03 18:02:07 +0000

    Remove xor_float.net example file

--- examples/simple_test.php
+++ examples/simple_test.php
@@ -1,5 +1,9 @@
 <?php
-$ann = fann_create_from_file(dirname(__FILE__) . "/xor_float.net");
+$train_file = (dirname(__FILE__) . "/xor_float.net");
+if (!is_file($train_file))
+	die("The file xor_float.net has not been created! Please run simple_train.php to generate it");
+
+$ann = fann_create_from_file($train_file);
 $input = array(-1, 1);
 $calc_out = fann_run($ann, $input);
 printf("xor test (%f,%f) -> %f\n", $input[0], $input[1], $calc_out[0]);
--- examples/simple_train.php
+++ examples/simple_train.php
@@ -14,7 +14,7 @@ if (is_resource($ann)) {
 	fann_set_activation_function_hidden($ann, FANN_SIGMOID_SYMMETRIC);
 	fann_set_activation_function_output($ann, FANN_SIGMOID_SYMMETRIC);
 
-	$filename =  dirname(__FILE__) . "/xor.data";
+	$filename = dirname(__FILE__) . "/xor.data";
 	if (fann_train_on_file($ann, $filename, $max_epochs, $epochs_between_reports, $desired_error))
 		fann_save($ann, dirname(__FILE__) . "/xor_float.net");
 
