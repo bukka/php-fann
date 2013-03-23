e7a17923f93f4293a83846753feb93fac148afbb
Jakub Zelenka <jakub.zelenka@gmail.com>
2013-03-23 18:03:22 +0000

    Add functions generation to the parameters generator

--- parameters_generator.php
+++ parameters_generator.php
@@ -31,7 +31,7 @@ function fpg_headers($name, $params) {
 }
 
 /* print function params definitions for reflection */
-function fpg_reflection($name, $params) {
+function fpg_reflections($name, $params) {
 	$begin = "ZEND_BEGIN_ARG_INFO(arginfo_fann_%s_$name, 0)";
 	$arg = "ZEND_ARG_INFO(0, %s)";
 	$end = "ZEND_END_ARG_INFO()";
@@ -57,6 +57,48 @@ function fpg_definitions($name, $params) {
 	fpg_printf($format, "get_$name,", "get");
 	if ($params['has_setter'])
 		fpg_printf($format, "set_$name,", "set");
+}
+
+/* print functions body */
+function fpg_functions($name, $params) {
+	$return_type = isset($params['return']) ? $params['return'] : reset($params['params']);
+	$return_cmd = 'RETURN_';
+	switch ($return_type) {
+	case 'float':
+	case 'double':
+	case 'fann_type':
+		$return_cmd .= 'DOUBLE';
+		break;
+	case 'int':
+		$return_cmd .= 'LONG';
+		break;
+	}
+	$getter = "
+/* {{{ proto bool fann_get_$name(resource ann)
+   Returns {$params['comment']} */
+PHP_FUNCTION(fann_get_$name)
+{
+	PHP_FANN_GET_PARAM(fann_get_$name, $return_cmd);
+}
+/* }}} */";
+	fpg_printf($getter);
+	if ($params['has_setter']) {
+		$set_args = "fann_set_$name";
+		$comment_args = "resource ann";
+		foreach ($params['params'] as $param_name => $param_type) {
+			$comment_args .= sprintf(", %s %s", $param_type == 'fann_type' ? 'double' : $param_type, $param_name);
+			$set_args .= sprintf(", %s", $param_type == 'int' ? 'l, long' : 'd, double');
+		}
+		$setter = "
+/* {{{ proto bool fann_set_$name($comment_args)
+   Sets {$params['comment']}*/
+PHP_FUNCTION(fann_set_$name)
+{
+    PHP_FANN_SET_PARAM($set_args);
+}
+/* }}} */";
+		fpg_printf($setter);
+	}
 }
 
 /* process parameters array */
