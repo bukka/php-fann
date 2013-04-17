ce06e8887499c4160de90333a47135bd36bf9768
Jakub Zelenka <jakub.zelenka@gmail.com>
2013-04-17 19:22:57 +0100

    Add support for multiparams to code generation in parameters generator

--- parameters_generator.php
+++ parameters_generator.php
@@ -3,99 +3,133 @@
 
 $training_params = array(
 	'training_algorithm' => array(
-        'params' => array('training_algorithm' => ''),
+        'params' => array('training_algorithm' => 'int'),
         'comment' => 'the training algorithm as described by fann_train_enum',
-        'test_param' => '',
+        'test_param' => 'FANN_TRAIN_QUICKPROP',
 	),
 	'learning_rate' => array(
-        'params' => array('learning_rate' => ''),
+        'params' => array('learning_rate' => 'float'),
         'comment' => 'the learning rate',
-        'test_param' => '',
+        'test_param' => '0.5',
 	),
 	'learning_momentum' => array(
-        'params' => array('learning_momentum' => ''),
+        'params' => array('learning_momentum' => 'float'),
         'comment' => 'the learning momentum',
-        'test_param' => '',
+        'test_param' => '0.2',
 	),
 	'activation_function' => array(
-        'params' => array('activation_function' => ''),
+        'params' => array('activation_function' => 'int', 'layer' => 'int', 'neuron' => 'int'),
         'comment' => 'the activation function for neuron number neuron in layer number layer, counting the input layer as layer 0',
-        'test_param' => '',
+        'test_param' => array('FANN_SIGMOID_SYMMETRIC', '1', '1'),
+		'empty_function' => true,
+	),
+	'activation_function_layer' => array(
+        'params' => array('activation_function' => 'int', 'layer' => 'int'),
+        'comment' => 'the activation function for all the neurons in the layer number layer, counting the input layer as layer 0',
+		'only_setter' => true,
+		'empty_function' => true,
+	),
+	'activation_function_hidden' => array(
+        'params' => array('activation_function' => 'int'),
+        'comment' => 'the activation function for all of the hidden layers',
+		'only_setter' => true,
+	),
+	'activation_function_output' => array(
+        'params' => array('activation_function' => 'int'),
+        'comment' => 'the activation function for the output layer',
+		'only_setter' => true,
 	),
 	'activation_steepness' => array(
-        'params' => array('activation_steepness' => ''),
+        'params' => array('activation_steepness' => 'fann_type', 'layer' => 'int', 'neuron' => 'int'),
         'comment' => 'the activation steepness for neuron number neuron in layer number layer, counting the input layer as layer 0',
-        'test_param' => '',
+		'test_param' => array('0.5', '1', '1'),
+		'empty_function' => true,
+	),
+	'activation_steepness_layer' => array(
+        'params' => array('activation_steepness' => 'int', 'layer' => 'int'),
+        'comment' => 'the activation steepness for all the neurons in the layer number layer, counting the input layer as layer 0',
+		'only_setter' => true,
+		'empty_function' => true,
+	),
+	'activation_steepness_hidden' => array(
+        'params' => array('activation_steepness' => 'int'),
+        'comment' => 'the activation steepness for all of the hidden layers',
+		'only_setter' => true,
+	),
+	'activation_steepness_output' => array(
+        'params' => array('activation_steepness' => 'int'),
+        'comment' => 'the activation steepness for the output layer',
+		'only_setter' => true,
 	),
 	'train_error_function' => array(
-        'params' => array('train_error_function' => ''),
+        'params' => array('train_error_function' => 'int'),
         'comment' => 'the error function used during training',
-        'test_param' => '',
+        'test_param' => 'FANN_ERRORFUNC_LINEAR',
 	),
 	'train_stop_function' => array(
-        'params' => array('train_stop_function' => ''),
+        'params' => array('train_stop_function' => 'int'),
         'comment' => 'the the stop function used during training',
-        'test_param' => '',
+        'test_param' => 'FANN_STOPFUNC_BIT',
 	),
 	'bit_fail_limit' => array(
-        'params' => array('bit_fail_limit' => ''),
+        'params' => array('bit_fail_limit' => 'fann_type'),
         'comment' => 'the bit fail limit used during training',
-        'test_param' => '',
+        'test_param' => '0.5',
 	),
 	'quickprop_decay' => array(
-        'params' => array('quickprop_decay' => ''),
+        'params' => array('quickprop_decay' => 'float'),
         'comment' => 'the decay is a small negative valued number which is the factor that the weights should become smaller in each iteration during quickprop training',
-        'test_param' => '',
+        'test_param' => '-0.0002',
 	),
 	'quickprop_mu' => array(
-        'params' => array('quickprop_mu' => ''),
+        'params' => array('quickprop_mu' => 'float'),
         'comment' => 'the mu factor is used to increase and decrease the step-size during quickprop training',
-        'test_param' => '',
+        'test_param' => '1.7',
 	),
 	'rprop_increase_factor' => array(
-        'params' => array('rprop_increase_factor' => ''),
+        'params' => array('rprop_increase_factor' => 'float'),
         'comment' => 'the increase factor is a value larger than 1, which is used to increase the step-size during RPROP training',
-        'test_param' => '',
+        'test_param' => '1.2',
 	),
 	'rprop_decrease_factor' => array(
-        'params' => array('rprop_decrease_factor' => ''),
+        'params' => array('rprop_decrease_factor' => 'float'),
         'comment' => 'the decrease factor is a value smaller than 1, which is used to decrease the step-size during RPROP training',
-        'test_param' => '',
+        'test_param' => '0.6',
 	),
 	'rprop_delta_min' => array(
-        'params' => array('rprop_delta_min' => ''),
+        'params' => array('rprop_delta_min' => 'float'),
         'comment' => 'the minimum step-size is a small positive number determining how small the minimum step-size may be',
-        'test_param' => '',
+        'test_param' => '0.1',
 	),
 	'rprop_delta_max' => array(
-        'params' => array('rprop_delta_max' => ''),
+        'params' => array('rprop_delta_max' => 'float'),
         'comment' => 'the maximum step-size is a positive number determining how large the maximum step-size may be',
-        'test_param' => '',
+        'test_param' => '49.5',
 	),
 	'rprop_delta_zero' => array(
-        'params' => array('rprop_delta_zero' => ''),
+        'params' => array('rprop_delta_zero' => 'float'),
         'comment' => 'the initial step-size is a positive number determining the initial step size',
-        'test_param' => '',
+        'test_param' => '0.2',
 	),
 	'sarprop_weight_decay_shift' => array(
-        'params' => array('sarprop_weight_decay_shift' => ''),
+        'params' => array('sarprop_weight_decay_shift' => 'float'),
         'comment' => 'the sarprop weight decay shift',
-        'test_param' => '',
+        'test_param' => '-6.744',
 	),
 	'sarprop_step_error_threshold_factor' => array(
-        'params' => array('sarprop_step_error_threshold_factor' => ''),
+        'params' => array('sarprop_step_error_threshold_factor' => 'float'),
         'comment' => 'the sarprop step error threshold factor',
-        'test_param' => '',
+        'test_param' => '0.2',
 	),
 	'sarprop_step_error_shift' => array(
-        'params' => array('sarprop_step_error_shift' => ''),
+        'params' => array('sarprop_step_error_shift' => 'float'),
         'comment' => 'the get sarprop step error shift',
-        'test_param' => '',
+        'test_param' => '1.4',
 	),
 	'sarprop_temperature' => array(
-        'params' => array('sarprop_temperature' => ''),
+        'params' => array('sarprop_temperature' => 'float'),
         'comment' => 'the sarprop weight decay shift',
-        'test_param' => '',
+        'test_param' => '0.02',
 	),
 );
 
@@ -193,7 +227,9 @@ function fpg_printf($format = "") {
 
 /* print function headers */
 function fpg_headers($name, $params) {
-	fpg_printf('PHP_FUNCTION(fann_get_%s);', $name);
+	if ($params['has_getter']) {
+		fpg_printf('PHP_FUNCTION(fann_get_%s);', $name);
+	}
 	if ($params['has_setter']) {
 		fpg_printf('PHP_FUNCTION(fann_set_%s);', $name);
 	}
@@ -205,10 +241,19 @@ function fpg_reflections($name, $params) {
 	$arg = "ZEND_ARG_INFO(0, %s)";
 	$end = "ZEND_END_ARG_INFO()";
 	// getter
-	fpg_printf($begin, 'get');
-	fpg_printf($arg, 'ann');
-	fpg_printf($end);
-	fpg_printf(); // EOL
+	if ($params['has_getter']) {
+		fpg_printf($begin, 'get');
+		fpg_printf($arg, 'ann');
+		$first = true;
+		foreach ($params['params'] as $param_name => $param_type) {
+			if ($first)
+				$first = false;
+			else
+				fpg_printf($arg, $param_name);
+		}
+		fpg_printf($end);
+		fpg_printf(); // EOL
+	}
 	// setter
 	if ($params['has_setter']) {
 		fpg_printf($begin, 'set');
@@ -224,7 +269,8 @@ function fpg_reflections($name, $params) {
 /* print function definitions for the module spec */
 function fpg_definitions($name, $params) {
 	$format = "\tPHP_FE(fann_%-41s arginfo_fann_%s_$name)";
-	fpg_printf($format, "get_$name,", "get");
+	if ($params['has_getter']) 
+		fpg_printf($format, "get_$name,", "get");
 	if ($params['has_setter'])
 		fpg_printf($format, "set_$name,", "set");
 }
@@ -243,8 +289,9 @@ function fpg_functions($name, $params) {
 		  $return_cmd .= 'LONG';
 		  break;
 	}
-	$body = $params['empty_function'] ? "": "PHP_FANN_GET_PARAM(fann_get_$name, $return_cmd);";
-	$getter = "
+	if ($params['has_getter']) {
+		$body = $params['empty_function'] ? "": "PHP_FANN_GET_PARAM(fann_get_$name, $return_cmd);";
+		$getter = "
 /* {{{ proto bool fann_get_$name(resource ann)
    Returns {$params['comment']} */
 PHP_FUNCTION(fann_get_$name)
@@ -252,7 +299,8 @@ PHP_FUNCTION(fann_get_$name)
 	$body
 }
 /* }}} */";
-	fpg_printf($getter);
+		fpg_printf($getter);
+	}
 	if ($params['has_setter']) {
 		$set_args = "fann_set_$name";
 		$comment_args = "resource ann";
@@ -277,6 +325,15 @@ PHP_FUNCTION(fann_set_$name)
 function fpg_tests($name, $params) {
 	$file_path = (dirname(__FILE__) . "/tests/fann_%s_{$name}_basic.phpt");
 	// getter
+	$test_params = isset($params['test_param']) ? $params['test_param'] : '';
+	if (is_array($test_params)) {
+		$setter_test_param = implode(', ', $test_params);
+		$setter_first_param = array_shift($test_params);
+		$getter_test_param = ', ' . implode(', ', $test_params);
+	} else {
+		$setter_test_param = $setter_first_param = $test_params;
+		$getter_test_param = '';
+	}
 	$getter_test_file = sprintf($file_path, 'get');
 	$getter_test_content = '--TEST--
 Test function fann_get_' . $name . '() by calling it with its expected arguments
@@ -285,7 +342,7 @@ Test function fann_get_' . $name . '() by calling it with its expected arguments
 
 $ann = fann_create_standard( 3, 3, 2, 1 );
 
-var_dump( fann_get_' . $name . '( $ann ) );
+var_dump( fann_get_' . $name . '( $ann' . $getter_test_param . ' ) );
 
 ?>
 --EXPECTF--
@@ -296,12 +353,12 @@ var_dump( fann_get_' . $name . '( $ann ) );
 	}
 	file_put_contents($getter_test_file, $getter_test_content);
 	// setter
-	if ($params['has_setter'] && isset($params['test_param'])) {
+	if ($params['has_setter'] && $setter_test_param) {
 		$setter_test_file = sprintf($file_path, 'set');
 		if (in_array($params['return'], array('float', 'double', 'fann_type')))
-			$setter_cmp_cond = 'round( fann_get_'  .$name . '( $ann ), 2 ) == round( ' . $params['test_param'] . ', 2 )';
+			$setter_cmp_cond = 'round( fann_get_'  .$name . '( $ann' . $getter_test_param . ' ), 2 ) == round( ' . $setter_first_param . ', 2 )';
 		else
-			$setter_cmp_cond = 'fann_get_'  .$name . '( $ann ) == ' . $params['test_param'];
+			$setter_cmp_cond = 'fann_get_'  . $name . '( $ann' . $getter_test_param . ' ) == ' . $setter_first_param;
 		$setter_test_content = '--TEST--
 Test function fann_set_' . $name . '() by calling it with its expected arguments
 --FILE--
@@ -309,7 +366,7 @@ Test function fann_set_' . $name . '() by calling it with its expected arguments
 
 $ann = fann_create_standard( 3, 3, 2, 1 );
 
-var_dump( fann_set_' . $name . '( $ann, ' . $params['test_param'] . ' ) );
+var_dump( fann_set_' . $name . '( $ann, ' . $setter_test_param . ' ) );
 var_dump( ' . $setter_cmp_cond . ' );
 
 ?>
@@ -330,6 +387,7 @@ function fpg_process_array($action, $params_list) {
 			$params['empty_function'] = isset($params['empty_function']) && $params['empty_function'];
 			$params['empty_test'] = isset($params['empty_test']) && $params['empty_test'];
 			$params['has_setter'] = !isset($params['only_getter']) || !$params['only_getter'];
+			$params['has_getter'] = !isset($params['only_setter']) || !$params['only_setter'];
 			$fce_name($name, $params);
 		}
 	}
@@ -343,5 +401,6 @@ if ($argc < 2)
 
 for ($i = 1; $i < $argc; $i++) {
 	fpg_process_array($argv[$i], $cascade_params);
+	fpg_process_array($argv[$i], $training_params);
 }
 
