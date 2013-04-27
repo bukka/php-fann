e03f68dfd34885e202860b16cfcf42577c8fb796
Jakub Zelenka <jakub.zelenka@gmail.com>
2013-04-27 20:02:37 +0100

    Improve script for checking tests to get additional test files

--- utils/check_tests.php
+++ utils/check_tests.php
@@ -3,8 +3,26 @@ $functions = get_extension_funcs('fann');
 if ($functions === false)
 	die("Fann extension is not loaded\n");
 
+$testdir = dirname(dirname(__FILE__)) . "/tests/";
+$testnames = array();
 foreach ($functions as $fce) {
-	$testfile = dirname(dirname(__FILE__)) . "/tests/${fce}_basic.phpt";
+	$testname = strtolower($fce)  . "_basic.phpt";
+	$testfile = $testdir . $testname;
 	if (!is_file($testfile))
 		echo "Missing test: $fce\n";
+	else
+		$testnames[] = $testname;
+}
+
+
+
+if ($dh = opendir($testdir)) {
+	while (($file = readdir($dh)) !== false) {
+		if ($file != '.' && $file != '..' && !in_array($file, $testnames))
+			echo "Additional test file: $file\n";
+	}
+	closedir($dh);
+}
+else {
+	die("Cannot open the test dir\n");
 }
