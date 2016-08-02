34f60ecc332f68c96c4b672388fb6a89984c6db6
Joy Harvel <geekgirl.joy@gmail.com>
2016-08-02 16:16:07 -0700

    Update test_all.php

--- examples/test_all.php
+++ examples/test_all.php
@@ -3,7 +3,7 @@
 function test($net, $test_data) {
 	$train_file = (dirname(__FILE__) . '/' . $net . '_float.net');
 	if (!is_file($train_file)) {
-		print('The file ' . $net . '_float.net has not been created! Please run train_all.php to generate it.<br>' . PHP_EOL);
+		print('The file ' . $net . '_float.net has not been created! Please run <a href="train_all.php">train_all.php</a> to generate it.<br>' . PHP_EOL);
 	} else{
 		$ann = fann_create_from_file($train_file);
 		if ($ann) {
