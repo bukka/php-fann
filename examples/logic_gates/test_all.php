df0d3efa407f0d757c59b821f466d9f8fd2d93b6
Joy Harvel <geekgirl.joy@gmail.com>
2016-08-18 03:10:48 -0700

    Update test_all.php

--- examples/logic_gates/test_all.php
+++ examples/logic_gates/test_all.php
@@ -1,9 +1,9 @@
 <?php
 
-function test($net, $test_data) {
-	$train_file = (dirname(__FILE__) . '/' . $net . '_float.net');
+function test($ann, $test_data) {
+	$train_file = (dirname(__FILE__) . '/' . $ann . '_float.net');
 	if (!is_file($train_file)) {
-		print('The file ' . $net . '_float.net has not been created! Please run train_all.php to generate it.<br>' . PHP_EOL);
+		print('The file ' . $ann . '_float.net has not been created! Please run train_all.php to generate it.<br>' . PHP_EOL);
 	} else{
 		$ann = fann_create_from_file($train_file);
 		if ($ann) {
@@ -12,7 +12,7 @@ function test($net, $test_data) {
 			$num_inputs = count($test_data);
 			$num_outputs = count($calc_out);
 			
-			$test_result = $net . ' test (';
+			$test_result = $ann . ' test (';
 			for($i = 0; $i < $num_inputs; $i++) {
 				$test_result .= $test_data[$i];
 
